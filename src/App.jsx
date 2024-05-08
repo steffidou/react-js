b34bce5e0a9be643ebcb6d5a53ab3ca9b6d093d8
Udasco, Dou Issa Steffi <160296940+steffidou@users.noreply.github.com>
2024-05-08 17:07:34 +0800

    haloha

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import Message from './components/Message'
-import MessageClass from './components/Messageclass'
+import React from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Content from './components/Content'
-import Sidebar from './components/sidebar'
-import Dashboard from './components/Dashboard'
-import Students from './components/Students'
-import Admin from './components/Admin'
-import TestComponents from './components/Testcomponents';
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-       
-      <Router>
-        <div className="flex flex-col h-screen w-screen">
-              <Header />
-                <div className="flex flex-1">
-                  <Sidebar />
-                    <Routes>
-                      <Route exact path="/dashboard" element={<Dashboard />} />
-                      <Route exact path="/admin" element={<Admin />} />
-                      <Route exact path="/students" element={<Students/>} />
-                    </Routes>
-                </div>
-              <Footer />
-          </div>
-       </Router>
-  )
+    <div>
+      <Header />
+      <div className="main-content">
+        <Sidebar />
+        {/* Main content of your application */}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
--- src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Message(props) {
-  return (
-    <div><h1>This is a function components</h1></div>
-  )
-}
-
-export default Message
